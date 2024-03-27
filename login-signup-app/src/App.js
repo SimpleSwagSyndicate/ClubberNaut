a6280c4033d9e855b1ce42911c6a29a97f71090d
Maxx417 <72826421+Maxx417@users.noreply.github.com>
2024-03-27 15:32:37 -0400

    create clubbernaut app layout

--- login-signup-app/src/App.js
+++ login-signup-app/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 import Login from "./components/Login/Login";
+import Signup from "./components/Signup/Signup"
 import { createTheme, MantineProvider, rem } from '@mantine/core';
 
 const theme = createTheme({
@@ -9,22 +10,17 @@ const theme = createTheme({
 });
 function App() {
   return (
-    <MantineProvider theme={theme}>
-  <Router>
+    <MantineProvider>
+    <Router>
       <div className="App">
-        <nav>
-          <ul>
-            <li>
-              <Link to = "/">Login</Link>
-            </li>
-          </ul>
-        </nav>
         <Routes>
           <Route exact="true" path="/" element={<Login  />}>
           </Route>
+          <Route exact="true" path="/" element={<Signup />}>
+          </Route>
         </Routes>
       </div>
-      </Router>
+    </Router>
     </MantineProvider>
       
   );
