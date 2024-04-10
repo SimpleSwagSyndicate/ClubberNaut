062fded87abc1ea8d92d7aaa4a522f6d7c74a60e
Maxx417 <72826421+Maxx417@users.noreply.github.com>
2024-04-10 19:20:11 -0400

    tag list, will implement grid later

--- clubbernaut/src/app/clubsearch/page.tsx
+++ clubbernaut/src/app/clubsearch/page.tsx
@@ -1,9 +1,21 @@
-import { Checklist } from '@/components/Checklist/Checklist'
-
+import { Checklist } from '@/components/Checklist/Checklist';
+import {
+    Container, Title, Anchor, Text, Paper, Grid
+} from '@mantine/core'
+import classes from './clubsearch.module.css'
 export default function clubsearch() {
   return (
-    <>
-    <Checklist />
-    </>
-  )
+    <div>
+        <>
+        <Checklist />
+        </>
+        <Container>
+            <Paper withBorder>
+                <Grid>
+
+                </Grid>
+            </Paper>
+        </Container>
+    </div>
+  );
 }
