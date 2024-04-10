b8da5e0783585ffd3eb6b308ba247caab0ec4702
Maxx417 <72826421+Maxx417@users.noreply.github.com>
2024-04-10 19:20:11 -0400

    tag list, will implement grid later

--- clubbernaut/src/components/Checklist/Checklist.tsx
+++ clubbernaut/src/components/Checklist/Checklist.tsx
@@ -1,15 +1,45 @@
-import { Checkbox, Group } from '@mantine/core';
+import { Checkbox, Stack, Title, Paper, Container, Grid } from '@mantine/core';
+import classes from './checklist.module.css';
 
 export function Checklist() {
+  // Need to make Grid dynamic eventually
   return (
-    <Checkbox.Group
-      label="Club Tags"
-    >
-      <Group mt="xs">
-        <Checkbox value="eSports" label="eSports" />
-        <Checkbox value="Awesome" label="Awesome" />
-        <Checkbox value="New Students" label="New Students" />
-      </Group>
-    </Checkbox.Group>
+    <div>
+      <Container size={250} className={classes.pos}>
+        <Paper withBorder shadow="md" p='md' mt='xl' radius="md">
+          <Stack className={classes.container}
+            gap='xl' 
+            >
+            <Title className={classes.title}>
+              Tags
+            </Title>
+            <Checkbox
+                  label="tag 1"
+            />
+            <Checkbox
+                  label="tag 2"
+            />
+            <Checkbox
+                  label="tag 3"
+            />
+            <Checkbox
+                  label="tag 4"
+            />
+            <Checkbox
+                  label="tag 5"
+            />
+            <Checkbox
+                  label="tag 6"
+            />
+            <Checkbox
+                  label="tag 7"
+            />
+            <Checkbox
+                  label="tag 8"
+            />
+          </Stack>
+        </Paper>
+      </Container>
+    </div>
   );
 }
