f9d88df638d25d868fd4b8cca20730fc2af326a2
Steven Arbo <steven.arbo@icloud.com>
2024-04-12 20:23:57 -0400

    create scrollable updates and add logo to homepage

--- clubbernaut/src/components/Hero/Hero.tsx
+++ clubbernaut/src/components/Hero/Hero.tsx
@@ -1,14 +1,59 @@
+'use client'
 
-import { Text } from '@mantine/core';
+import Image from 'next/image'
+import { Paper, ScrollArea, Text, Title , Space} from '@mantine/core';
 import classes from './Hero.module.css';
+
+
 
 
 export function Hero() {
   return (
-  
-   <Text className={classes.title}>
-     Welcome to Clubbernaut!
-   </Text>
 
+     <div>
+     <Title className={classes.title}>
+       Welcome to
+     </Title>
+     <Title className={classes.update}>
+      Here's what's going on in your communities...
+     </Title>
+     <div>
+      <Image
+       className={classes.logo}
+       src='/clubbernaut-high-resolution-logo-transparent.png'
+       alt='Logo'
+       width={500}
+       height={500}
+       >
+       </Image>
+     <div className={classes.scroll}>
+     <ScrollArea w={600} h={450}>
+      <Paper style={{backgroundColor: "#FFFFFF"}} shadow="lg" withBorder p={50} radius={50}>
+        <Text c='black'> This is an update</Text>
+      </Paper>
+      <Space h="md" />
+      <Paper style={{backgroundColor: "#FFFFFF"}} shadow="lg" withBorder p={50} radius={50}>
+        <Text c='black'> This is an update</Text>
+      </Paper>
+      <Space h="md" />
+      <Paper style={{backgroundColor: "#FFFFFF"}} shadow="lg" withBorder p={50} radius={50}>
+        <Text c='black'> This is an update</Text>
+      </Paper>
+      <Space h="md" />
+      <Paper style={{backgroundColor: "#FFFFFF"}} shadow="lg" withBorder p={50} radius={50}>
+        <Text c='black'> This is an update</Text>
+      </Paper>
+      <Space h="md" />
+      <Paper style={{backgroundColor: "#FFFFFF"}} shadow="lg" withBorder p={50} radius={50}>
+        <Text c='black'> This is an update</Text>
+      </Paper>
+      <Space h="md" />
+      <Paper style={{backgroundColor: "#FFFFFF"}} shadow="lg" withBorder p={50} radius={50}>
+        <Text c='black'> This is an update</Text>
+      </Paper>
+     </ScrollArea>
+     </div>
+     </div>
+     </div>
   );
 }
