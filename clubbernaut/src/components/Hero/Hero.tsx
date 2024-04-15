c08c15e6ba7b9b54148b22296744a6676fb22856
Steven Arbo <steven.arbo@icloud.com>
2024-04-15 13:07:49 -0400

    working with logo

--- clubbernaut/src/components/Hero/Hero.tsx
+++ clubbernaut/src/components/Hero/Hero.tsx
@@ -1,7 +1,6 @@
 'use client'
 
-import Image from 'next/image'
-import { Paper, ScrollArea, Text, Title , Space} from '@mantine/core';
+import { Image, Paper, ScrollArea, Text, Title , Space} from '@mantine/core';
 import classes from './Hero.module.css';
 
 
@@ -10,10 +9,7 @@ import classes from './Hero.module.css';
 export function Hero() {
   return (
 
-     <div>
-     <Title className={classes.title}>
-       Welcome to
-     </Title>
+    <div>
      <Title className={classes.update}>
       Here's what's going on in your communities...
      </Title>
@@ -22,10 +18,7 @@ export function Hero() {
        className={classes.logo}
        src='/clubbernaut-high-resolution-logo-transparent.png'
        alt='Logo'
-       width={500}
-       height={500}
-       >
-       </Image>
+       />
      <div className={classes.scroll}>
      <ScrollArea w={600} h={450}>
       <Paper style={{backgroundColor: "#FFFFFF"}} shadow="lg" withBorder p={50} radius={50}>
