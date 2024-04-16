daf8944a1d4b2963a7a9ae12a11c9469781bdc76
dmkhang1101 <114703634+dmkhang1101@users.noreply.github.com>
2024-04-16 14:41:03 -0400

    Update Signup.js

--- clubbernaut/src/app/signup/Signup.js
+++ clubbernaut/src/app/signup/Signup.js
@@ -1,4 +1,5 @@
 const {createClient} = require('@supabase/supabase-js')
+const uuid = require('uuid')
 const supabaseURL = '***REMOVED***'
 const supabaseKEY = '***REMOVED***'
 const supabase = createClient(supabaseURL,supabaseKEY)
@@ -13,27 +14,29 @@ const SignUp = async (user_name, user_password, user_confirm_password, user_emai
         return {"status" : "Error","msg": "Invalid email address."};
     }
 
-    const {exist, error_exist} = await supabase
-    .from('User Profile')
-    .select('email')
+    const {data: exist, error_exist} = await supabase
+    .from('User Profile V1a')
+    .select('*')
     .eq('email',user_email)
 
     if (error_exist){
         return {"status":"Error","msg":`${error_exist}`}
     }
-    if (exist !== "undefined") {
+    if (exist.length() !== 0) {
         return {"status":"Error","msg": "Email already exist"}
     }
 
     const {data_insert, error_insert} = await supabase
-    .from('User Profile')
+    .from('User Profile V1a')
     .insert({
+        userid : parseInt(uuid.v4()),
         name : user_name,
         password : user_password,
         email : user_email,
         clubs : [],
         notification : true,
     })
+    
     if (error_insert) {
         return {"status":"Error","msg":`${error_insert}`}
     }
