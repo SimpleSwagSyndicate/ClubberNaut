fc445cec316bd1844ce42704877d03c824ddab2a
Maxx417 <72826421+Maxx417@users.noreply.github.com>
2024-04-24 15:41:06 -0400

    create clubhome page

--- clubbernaut/src/app/personal/page.tsx
+++ clubbernaut/src/app/personal/page.tsx
@@ -5,7 +5,6 @@ import { Title, Container, Stack, Paper, Group, Button } from '@mantine/core'
 export default function personal() {
     return (
         <div>
-            <Button component={Link} href='/clubcreation' variant="default">Create Club</Button>
             <Group>
             <Title className={classes.title}>
                 Your Clubs
@@ -23,6 +22,7 @@ export default function personal() {
                     <Paper style={{backgroundColor: "#971B2F"}} shadow="lg" p={50} radius={50} >
                         Club 4
                     </Paper>
+                    <Button color='#971B2F' component={Link} href='/clubcreation' variant="default">Create Club</Button>
                 </Stack>
             </Group>
             <Container className={classes.border} />
