a2fb1ce3c6d4a9ed15c49554b2c1e71b9a7801e5
Steven Arbo <steven.arbo@icloud.com>
2024-04-24 15:42:01 -0400

    scrollable clubs

--- clubbernaut/src/app/personal/page.tsx
+++ clubbernaut/src/app/personal/page.tsx
@@ -1,7 +1,7 @@
 import { Component } from 'react'
 import classes from './personal.module.css'
 import Link from 'next/link';
-import { Title, Container, Stack, Paper, Group, Button } from '@mantine/core'
+import { Text, ScrollArea, Title, Container, Stack, Paper, Group, Button, Space } from '@mantine/core'
 export default function personal() {
     return (
         <div>
@@ -10,20 +10,27 @@ export default function personal() {
             <Title className={classes.title}>
                 Your Clubs
             </Title>
-                <Stack className={classes.listClubs}>
-                    <Paper className={classes.clubCard} style={{backgroundColor: "#971B2F"}} shadow="lg" p={50} radius={50} >
-                        Club 1
+                <ScrollArea className={classes.scroll} w={300} h={500}>
+                    <Paper style={{backgroundColor: "#971B2F"}} shadow="lg" p={50} radius={50}>
+                        <Text> Club 1</Text>
                     </Paper>
-                    <Paper style={{backgroundColor: "#971B2F"}} shadow="lg" p={50} radius={50} >
-                        Club 2
+                    <Space h='md'/>
+                    <Paper style={{backgroundColor: "#971B2F"}} shadow="lg" p={50} radius={50}>
+                        <Text> Club 2</Text>
                     </Paper>
-                    <Paper style={{backgroundColor: "#971B2F"}} shadow="lg" p={50} radius={50} >
-                        Club 3
+                    <Space h='md'/>
+                    <Paper style={{backgroundColor: "#971B2F"}} shadow="lg" p={50} radius={50}>
+                        <Text> Club 3</Text>
                     </Paper>
-                    <Paper style={{backgroundColor: "#971B2F"}} shadow="lg" p={50} radius={50} >
-                        Club 4
+                    <Space h='md'/>
+                    <Paper style={{backgroundColor: "#971B2F"}} shadow="lg" p={50} radius={50}>
+                        <Text> Club 4</Text>
                     </Paper>
-                </Stack>
+                    <Space h='md'/>
+                    <Paper style={{backgroundColor: "#971B2F"}} shadow="lg" p={50} radius={50}>
+                        <Text> Club 5</Text>
+                    </Paper>
+                </ScrollArea>
             </Group>
             <Container className={classes.border} />
         </div>
