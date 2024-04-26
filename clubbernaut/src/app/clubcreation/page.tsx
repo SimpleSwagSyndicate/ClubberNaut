36450864dee87c30ac84a10afca7895881b8758b
dmkhang1101 <114703634+dmkhang1101@users.noreply.github.com>
2024-04-26 11:44:27 -0400

    created and integrated create club function

--- clubbernaut/src/app/clubcreation/page.tsx
+++ clubbernaut/src/app/clubcreation/page.tsx
@@ -1,6 +1,23 @@
+'use client'
+
 import { Paper, Stack, TextInput, Textarea, Container, Group, Title, Button, Checkbox} from '@mantine/core';
 import classes from './clubcreation.module.css'
+import MakeClub from './clubcreate'
+import React, { useState, useEffect} from 'react';
+import { getTags, handleCheck } from './handleCheck'
+import { TagBox } from './CheckBox';
+
 export default function clubcreation() {
+
+  const [club_name,setClubName] = useState('')
+  const [club_description,setClubDescription] = useState('')
+
+  const handleCreateClub = async () => {
+    const res = await MakeClub(club_name, getTags(),club_description);
+    alert(res.msg)
+    console.log()
+  }
+
   return (
     <div>
         <Title className={classes.title} >
@@ -15,22 +32,23 @@ export default function clubcreation() {
           placeholder="Club Name"
           variant="filled"
           className={classes.section}
+          onChange={(e)=>setClubName(e.target.value)}
         />
 
       <Group align='center'  className={classes.section}>
-        <Checkbox label='Computer Science'/>
-        <Checkbox label='AI'/>
-        <Checkbox label='Security'/>
-        <Checkbox label='Programming'/>
-        <Checkbox label='Gaming'/>
-        <Checkbox label='Esports'/>
-        <Checkbox label='English'/>
-        <Checkbox label='Literature'/>
-        <Checkbox label='Books'/>
-        <Checkbox label='Outdoor'/>
-        <Checkbox label='Music'/>
-        <Checkbox label='Sport'/>
-        <Checkbox label='Adventure'/>
+        <TagBox label={'Computer Science'}handleCheck={handleCheck}/>
+        <TagBox label={'AI'}handleCheck={handleCheck}/>
+        <TagBox label={'Security'}handleCheck={handleCheck}/>
+        <TagBox label={'Programming'}handleCheck={handleCheck}/>
+        <TagBox label={'Gaming'}handleCheck={handleCheck}/>
+        <TagBox label={'Esports'}handleCheck={handleCheck}/>
+        <TagBox label={'English'}handleCheck={handleCheck}/>
+        <TagBox label={'Literature'}handleCheck={handleCheck}/>
+        <TagBox label={'Books'}handleCheck={handleCheck}/>
+        <TagBox label={'Outdoor'}handleCheck={handleCheck}/>
+        <TagBox label={'Music'}handleCheck={handleCheck}/>
+        <TagBox label={'Sport'}handleCheck={handleCheck}/>
+        <TagBox label={'Adventure'}handleCheck={handleCheck}/>
       </Group>
       </Stack>
       <Textarea 
@@ -43,9 +61,10 @@ export default function clubcreation() {
         name="message"
         variant="filled"
         className={classes.section}
+        onChange={(e) => setClubDescription(e.target.value)}
       />
       <Group justify="center" mt="xl" className={classes.button}>
-        <Button type="submit" size="lg" color='#971B2F'>
+        <Button type = "submit" onClick={handleCreateClub} size="lg" color='#971B2F'>
           Create Club
         </Button>
       </Group>
