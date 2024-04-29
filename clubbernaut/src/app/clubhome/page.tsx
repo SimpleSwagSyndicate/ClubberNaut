80e3f9b58429f719a06801c14605f182472829a5
Maxx417 <72826421+Maxx417@users.noreply.github.com>
2024-04-29 15:07:15 -0400

    Create clubhome page
    Also anchored it to badgecard club names.

---
+++ clubbernaut/src/app/clubhome/page.tsx
@@ -0,0 +1,11 @@
+import classes from './clubhome.module.css'
+import { Title, Input, Image } from '@mantine/core'
+
+export default function clubhome(id: any) {
+    return (
+        <div>
+            <Title></Title>
+        </div>
+    )
+}
+
