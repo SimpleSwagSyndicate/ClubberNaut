08e1626967e165af2d2f0cd987693e6f4cf17126
Anirudh Sathiya <anirudh.sathiya@gmail.com>
2024-04-30 18:15:39 -0400

    once logged in, header shows welcome message

--- clubbernaut/src/components/Header/Header.tsx
+++ clubbernaut/src/components/Header/Header.tsx
@@ -12,11 +12,58 @@ import {
   import cx from 'clsx';
   import classes from './Header.module.css';
   import Link from 'next/link';
+  import { UserContext, UserInfo } from '@/controllers/UserInfo';
+  import { useContext } from 'react';
+import { useEffect, useState } from 'react';
+  const {createClient} = require('@supabase/supabase-js')
+  const supabaseURL = '***REMOVED***'
+  const supabaseKEY = '***REMOVED***'
+  const supabase = createClient(supabaseURL,supabaseKEY)
   
+  // Function to fetch username based on email
+  const fetchUsernameByEmail = async (email) => {
+    try {
+      const { data, error } = await supabase
+        .from('User Profile V1a')
+        .select('name')
+        .eq('email', email)
+        .single(); // Assuming there is only one user with the given email
+  
+      if (error) {
+        throw error;
+      }
+      
+      if (data) {
+        return data.name; // Return the username if found
+      } else {
+        return null; // Return null if no user found with the given email
+      }
+    } catch (error) {
+      console.error('Error fetching username:', error.message);
+      return null;
+    }
+  };
+
   export function Header() {
+    const user = useContext(UserContext);
+    const [email, setEmail] = [user.email, user.setEmail];
+    let [name, setName] = useState('');
+
+    useEffect(() => {
+      const fetch = async () => {
+
+        let Name = await fetchUsernameByEmail(email)
+        setName(Name)
+      }
+      fetch()
+    },[email])
+
+
+
     const { setColorScheme } = useMantineColorScheme();
     const computedColorScheme = useComputedColorScheme('dark', { getInitialValueInEffect: true })
     return (
+      <UserInfo>
       <Box pb={120}>
         <header className={classes.header}>
           <Group justify="space-between" h="100%">
@@ -47,12 +94,21 @@ import {
               <Text className={cx(classes.icon, classes.light)}>L</Text>
               <Text className={cx(classes.icon, classes.dark)}>D</Text>
               </ActionIcon>
+              <div>
+      {email ? (
+        <span>Welcome, {name}</span>
+      ) : (
+        <>
               <Button component={Link} href='/login' variant="default">Log in</Button>
               <Button color='#971B2F' component={Link} href='signup'>Sign up</Button>
+              </>
+      )}
+    </div>
             </Group>
   
           </Group>
         </header>
       </Box>
+      </UserInfo>
     );
   }
