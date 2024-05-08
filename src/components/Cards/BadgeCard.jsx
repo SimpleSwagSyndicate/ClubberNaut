81aca76e6dd51ca73ecdd7d25a5f11147aec6f73
Kolyn090 <78561131+Kolyn090@users.noreply.github.com>
2024-05-08 17:37:25 -0400

    Merge cleaned create app branch (#19)
    * Frontend setup & login/signup page
    
    * Add Max to names
    
    * Commit login/signup page
    
    Very barebones, will customize later.
    
    * Change structure of file
    
    * create clubbernaut app layout
    
    * add login.module.css
    
    ---------
    
    Co-authored-by: Kolyn090 <78561131+Kolyn090@users.noreply.github.com>
    
    * remove node modules
    
    * fix node modules
    
    * Fixed build crush, unified package manager to npm, changed project hierarchy (moved everything out from clubbernaut directory), use 'npm run build' and 'npm run dev' to run the project.
    
    * Removed old node modules
    
    * Fixed a typo in gitignore
    
    * Update README.md
    
    * Styled the project
    
    * Create net.config.js
    
    * Populated Club Page
    
    * Update page.tsx
    
    * changes
    
    * final changes
    
    * Going to fix folder hierarchy
    
    * Commit to merge khang branch
    
    * rerouted club to their club page
    
    * Added items to ignore file
    
    * Removed config files
    
    * Added config to ignore
    
    ---------
    
    Co-authored-by: Maxx417 <72826421+Maxx417@users.noreply.github.com>
    Co-authored-by: dmkhang1101 <114703634+dmkhang1101@users.noreply.github.com>

---
+++ src/components/Cards/BadgeCard.jsx
@@ -0,0 +1,72 @@
+import { Card, Image, Text, Group, Badge, Button, CardSection, Anchor } from '@mantine/core';
+import classes from './BadgeCard.module.css';
+import { Link } from 'next/link';
+import { UserContext } from '@/controllers/UserInfo';
+import { useContext, useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
+
+const { createClient } = require('@supabase/supabase-js');
+const supabaseURL = '***REMOVED***';
+const supabaseKEY = '***REMOVED***'
+const supabase = createClient(supabaseURL, supabaseKEY);
+
+const retrieve_club_id = async (club_name) => {
+  const {data: clubid, error} = await supabase
+    .from('Club Profile')
+    .select('clubid')
+    .eq('name',club_name)
+  const club_id = clubid[0].clubid
+  return club_id
+}
+
+export const BadgeCard = props => {
+  const user = useContext(UserContext);
+  const features = props.tags.map(tag => (
+    <Badge color="white" variant="light" key={tag}>
+      {tag}
+    </Badge>
+  ));
+  const router = useRouter();
+  return (
+    <Card withBorder radius="md" p="md" className={classes.card}>
+      <CardSection>
+        <Image src={props.image} alt={props.name} height={150} />
+      </CardSection>
+
+      <CardSection className={classes.section} mt="md">
+        <Group justify="apart">
+          <Anchor
+            variant="gradient"
+            gradient={{ from: 'white', to: '#971B2F' }}
+            fz="lg"
+            fw={500}
+            onClick={async () => {
+              const club_id = await retrieve_club_id(props.name)
+              user.updateClubId(club_id)
+              router.push('/clubhome');
+            }}
+            underline="never"
+          >
+            {props.name}
+          </Anchor>
+        </Group>
+      </CardSection>
+
+      <CardSection className={classes.tagsection}>
+        <Text mt="md" className={classes.label} c="dimmed">
+          Perfect for you, if you enjoy
+        </Text>
+        <Group gap={7} mt={5}>
+          {features}
+        </Group>
+      </CardSection>
+      <CardSection className={classes.buttonsection}>
+        <Group mt="xs">
+          <Button color="#971B2F" radius="md" style={{ flex: 1 }}>
+            Follow
+          </Button>
+        </Group>
+      </CardSection>
+    </Card>
+  );
+};
