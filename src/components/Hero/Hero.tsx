eec3d4931825857c933314602f2a10352d29d70d
Kolyn090 <78561131+Kolyn090@users.noreply.github.com>
2024-05-08 17:37:25 -0400

    Merge cleaned create app branch (#19)
    * Frontend setup & login/signup page
    
    * Add Max to names
    
    * Commit login/signup page
    
    Very barebones, will customize later.
    
    * Change structure of file
    
    * create clubbernaut app layout
    
    * add login.module.css
    
    ---------
    
    Co-authored-by: Kolyn090 <78561131+Kolyn090@users.noreply.github.com>
    
    * remove node modules
    
    * fix node modules
    
    * Fixed build crush, unified package manager to npm, changed project hierarchy (moved everything out from clubbernaut directory), use 'npm run build' and 'npm run dev' to run the project.
    
    * Removed old node modules
    
    * Fixed a typo in gitignore
    
    * Update README.md
    
    * Styled the project
    
    * Create net.config.js
    
    * Populated Club Page
    
    * Update page.tsx
    
    * changes
    
    * final changes
    
    * Going to fix folder hierarchy
    
    * Commit to merge khang branch
    
    * rerouted club to their club page
    
    * Added items to ignore file
    
    * Removed config files
    
    * Added config to ignore
    
    ---------
    
    Co-authored-by: Maxx417 <72826421+Maxx417@users.noreply.github.com>
    Co-authored-by: dmkhang1101 <114703634+dmkhang1101@users.noreply.github.com>

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,107 @@
+'use client';
+
+import { Image, Paper, ScrollArea, Text, Title, Space } from '@mantine/core';
+import classes from './Hero.module.css';
+import { useContext, useEffect, useState } from 'react';
+import { UserContext } from '../../controllers/UserInfo';
+import { resolve } from 'path';
+
+const { createClient } = require('@supabase/supabase-js');
+const supabaseURL = 'https://fricdlpilwnfjdmtvvle.supabase.co';
+const supabaseKEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZyaWNkbHBpbHduZmpkbXR2dmxlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDg2MzQ1NzEsImV4cCI6MjAyNDIxMDU3MX0.PWWh9fYaHNEOoEC61p7k4TcdmrYwe-M5EWV5mwBC-Xk'
+const supabase = createClient(supabaseURL, supabaseKEY);
+
+const retrieve_updates = async (user_id: number) => {
+  const { data: clubs, error } = await supabase
+    .from('User Profile V1a')
+    .select('clubs')
+    .eq('userid', user_id);
+
+  const user_clubs = clubs[0].clubs;
+  const updates = [];
+  for (let i = 0; i < user_clubs.length; i++) {
+    const club_name = user_clubs[i];
+
+    const { data: update, error } = await supabase
+      .from('Club Profile')
+      .select('recent_update')
+      .eq('name', club_name);
+
+    if (update[0] === undefined) {
+      continue;
+    }
+    const club_updates = update[0].recent_update;
+
+    updates.push([club_name, club_updates[0]]);
+  }
+  return updates;
+};
+
+export function Hero() {
+  const user = useContext(UserContext);
+  const [updates, setUpdates] = useState<any[][]>([]);
+
+  useEffect(() => {
+    console.log('USER ID: ' + user.userid);
+    const fetch = async () => {
+      let updates = await retrieve_updates(user.userid);
+      setUpdates(updates);
+    };
+    fetch();
+  }, [user.userid]);
+
+  const content = updates
+    .map(u => {
+      const club = u[0];
+      const content = u[1];
+      if (content == null) {
+        return [];
+      }
+      const content_with_club = Object.values(content).map((c: any) => ({ ...c, club: club }));
+      return content_with_club;
+    })
+    .flat();
+
+  return (
+    <div>
+      <Title className={classes.update}>Here's what's going on in your communities...</Title>
+      <div>
+        <Image
+          className={classes.logo}
+          src="/clubbernaut-high-resolution-logo-transparent.png"
+          alt="Logo"
+        />
+
+        <div className={classes.scroll}>
+          <ScrollArea w={600} h={600}>
+            {content.map((c: any, index) => (
+              <div key={index}>
+                <Paper style={{ backgroundColor: '#971B2F' }} shadow="lg" p={50} radius={50}>
+                  <Text c="#FFFFFF" className={classes.club}>
+                    {' '}
+                    {c.club}{' '}
+                  </Text>
+                  <br></br>
+                  <Text c="#FFFFFF" className={classes.content}>
+                    {' '}
+                    {c.date}{' '}
+                  </Text>
+                  <Text c="#FFFFFF" className={classes.content}>
+                    {' '}
+                    {c.title}{' '}
+                  </Text>
+                  <br></br>
+                  <Text c="#FFFFFF" className={classes.content}>
+                    {' '}
+                    {c.description}{' '}
+                  </Text>
+                </Paper>
+                <Space h="md" />
+              </div>
+            ))}
+          </ScrollArea>
+        </div>
+      </div>
+    </div>
+  );
+}
