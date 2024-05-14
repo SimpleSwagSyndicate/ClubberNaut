66ddca2a4611e53d9716e07f966328a4276fa959
dmkhang1101 <114703634+dmkhang1101@users.noreply.github.com>
2024-05-13 22:07:27 -0400

    Added link to club page for home page's update bars

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -4,6 +4,7 @@ import { Image, Paper, ScrollArea, Text, Title, Space } from '@mantine/core';
 import classes from './Hero.module.css';
 import { useContext, useEffect, useState } from 'react';
 import { UserContext } from '../../controllers/UserInfo';
+import { useRouter } from 'next/navigation';
 import { resolve } from 'path';
 
 const { createClient } = require('@supabase/supabase-js');
@@ -11,15 +12,28 @@ const supabaseURL = process.env.SUPABASE_URL;
 const supabaseKEY = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseURL, supabaseKEY);
 
+const retrieve_club_id = async (club_name:string) => {
+  const {data: clubid, error} = await supabase
+    .from('Club Profile')
+    .select('clubid')
+    .eq('name',club_name)
+  const club_id = clubid[0].clubid
+  return club_id
+}
+
 const retrieve_updates = async (user_id: number) => {
   const { data: clubs, error } = await supabase
     .from('User Profile V1a')
     .select('clubs')
     .eq('userid', user_id);
+  
+  if (clubs === null){
+    return [];
+  }
 
-  const user_clubs = clubs[0].clubs;
+  const user_clubs = clubs[0]?.clubs;
   const updates = [];
-  for (let i = 0; i < user_clubs.length; i++) {
+  for (let i = 0; i < user_clubs?.length; i++) {
     const club_name = user_clubs[i];
 
     const { data: update, error } = await supabase
@@ -61,6 +75,8 @@ export function Hero() {
       return content_with_club;
     })
     .flat();
+    
+  const router = useRouter();
 
   return (
     <div>
@@ -76,7 +92,11 @@ export function Hero() {
           <ScrollArea w={600} h={600}>
             {content.map((c: any, index) => (
               <div key={index}>
-                <Paper style={{ backgroundColor: '#971B2F' }} shadow="lg" p={50} radius={50}>
+                <Paper style={{ backgroundColor: '#971B2F' }} onClick={async () => {
+              const club_id = await retrieve_club_id(c.club)
+              user.updateClubId(club_id)
+              router.push('/clubhome');
+            }} shadow="lg" p={50} radius={50}>
                   <Text c="#FFFFFF" className={classes.club}>
                     {' '}
                     {c.club}{' '}
